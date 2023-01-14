27573e6e86cbf7a8d02db1d9557adc8758dea216
Alexmerser54 <alexmerser541@gmail.com>
2023-01-14 14:00:28 +0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Laba6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(193, 154);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "9.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Кол-во слов: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "9.2";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(286, 34);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(193, 154);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 463);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Выполнить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "2.1";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 300);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(193, 154);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(286, 463);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Выполнить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "2.2";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(286, 300);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(193, 154);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = resources.GetString("richTextBox4.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 543);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Laba6
+{
+    public partial class Form1 : Form
+    {
+
+        char[] consonants = {'й', 'ц', 'к', 'н', 'г', 'ш', 'щ', 'з', 'х',
+                              'ф','в','п','р','л','д','ж',
+                              'ч','с','м','т','б' }; // согласные буквы
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = richTextBox1.Text;
+            string[] words; // массив слов
+            richTextBox1.Text = "";
+
+            Regex regex = new Regex(@"/[!?,-.]"); // шаблон удаления знаков препинания
+            int count = Convert.ToInt32(textBox1.Text); // количество слов
+            string[] sentences = input.Split(new string[] { ". " }, StringSplitOptions.RemoveEmptyEntries); // разбить текст на предложения
+            foreach (var item in sentences) // перебор предложений
+            {
+                words = regex.Replace(item, "").Split(' '); // удалить знаки препинания и разбить предложение на слова
+                if (words.Length == count) // если количество слов совпадает с введённым
+                    richTextBox1.Text += item + ". "; // вывести предложение с точкой
+            }
+
+        }
+
+        bool ContainsEveryChar(string inp, char[] arr) // метод проверки строки на наличие каждого символа из массива
+        {
+            string str = inp.ToLower(); // всё приводим к нижнему регистру
+            foreach (var item in arr) // перебор символов массива
+            {
+                if (!str.Contains(item)) // если строка не содержит символов
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool ContainsAnyChar(string inp, char[] arr) // метод проверки строки на наличие любого символа из массива
+        {
+            string str = inp.ToLower(); // всё приводим к нижнему регистру
+            foreach (var item in arr) // перебор симолов массива
+            {
+                if (str.Contains(item)) // если строка содержит символа
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool EqualAnyChar(char inp, char[] arr) // метод проверки равенства символа на любой символ в массиве символов
+        {
+            foreach (var ch in arr)
+            {
+                if (inp == ch)
+                    return true;
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = richTextBox2.Text;
+            richTextBox2.Text = "";
+            string[] words = input.Split(' '); // разбить предложение на слова
+            char[] letters = words[0].ToLower().ToCharArray(); // преобразовать первое слово в массив симолов
+
+            for (int i = 1; i < words.Length; i++) // перебор слов, не включая первого
+            {
+                if (ContainsEveryChar(words[i], letters)) // если слово содержит символы массива letters
+                    richTextBox2.Text += words[i] + " "; // вывести слово и пробел
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string word; 
+            string input = richTextBox3.Text;
+            richTextBox3.Text = "";
+
+            Regex regex = new Regex(@"\w+", RegexOptions.IgnoreCase); // шаблон поиска слов
+            foreach (var item in regex.Matches(input)) // перебор найденных строк
+            {
+                word = item.ToString().ToLower(); // преобразуем каждое слово в нижний регистр
+                if (EqualAnyChar(word[0], consonants)) // если первая буква слова равна любому из символов в массиве consonants
+                    richTextBox3.Text += item + " "; // вывести слово вместе с пробелом
+            }
+
+        }   
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string input = richTextBox4.Text;
+            richTextBox4.Text = "";
+
+            Regex spacesRegex = new Regex(@"\s+"); // шаблон поиска множества пробелов
+            Regex tabulationRegex = new Regex(@"(?:[.?!])(\s*)"); // шаблон поиска всех знаков окончания предложения, не включая их самих в выборку
+                                                                  // и любого количества пробелов
+
+            input = spacesRegex.Replace(input, " "); // замена множества пробелов на один
+            input = tabulationRegex.Replace(input, m => m.Groups[0].Value[0].ToString() + "  ");  // заменить все вхождения на конкатенацию первого симовола первой найденной группы и двух пробелов
+
+            richTextBox4.Text = input; // вывести результат
+        }
+    }
+}
